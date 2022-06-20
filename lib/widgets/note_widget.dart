c58cb89276ac4b646c98e893f072a488d97d1585
TDuffinNTU <thomaslukeduffin@hotmail.co.uk>
2022-06-20 21:21:06 +0100

    attempt to integrate GNOME context menu

--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
+import 'package:contextual_menu/contextual_menu.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart' hide MenuItem;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:libadwaita/libadwaita.dart';
+import 'package:my_app/models/notes_model.dart';
 
 import '../screens/edit_note_screen.dart';
 
-class NoteWidget extends StatelessWidget {
-  const NoteWidget({
+class NoteWidget extends ConsumerWidget {
+  NoteWidget({
     Key? key,
     required this.context,
     required this.index,
@@ -18,24 +22,68 @@ class NoteWidget extends StatelessWidget {
   final Animation<double> animation;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool rightClickEvent = false;
     return FadeTransition(
       opacity: animation,
       child: SizeTransition(
         sizeFactor: animation,
-        child: AdwActionRow(
-          title: content,
-          end: IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {},
-          ),
-          onActivated: () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => EditNoteScreen(index: index),
+        child: Listener(
+          onPointerDown: (event) {
+            rightClickEvent = event.kind == PointerDeviceKind.mouse &&
+                event.buttons == kSecondaryMouseButton;
+          },
+          onPointerUp: (event) async {
+            if (rightClickEvent) {
+              await popUpContextualMenu(_menu,
+                  position: Offset(
+                    event.position.dx,
+                    event.position.dy,
+                  ),
+                  placement: Placement.bottomLeft);
+            }
+          },
+          child: AdwActionRow(
+            title: content,
+            end: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                ref.read(notesProvider.notifier).delNote(index);
+              },
+            ),
+            onActivated: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => EditNoteScreen(index: index),
+              ),
             ),
           ),
         ),
       ),
     );
   }
+
+  final Menu _menu = Menu(
+    items: [
+      MenuItem(
+        label: 'Copy',
+        onClick: (_) {
+          print('Clicked Copy');
+        },
+      ),
+      MenuItem(
+        label: 'Disabled item',
+        disabled: true,
+      ),
+      MenuItem.checkbox(
+        key: 'checkbox1',
+        label: 'Checkbox1',
+        checked: true,
+        onClick: (menuItem) {
+          print('Clicked Checkbox1');
+          menuItem.checked = !(menuItem.checked == true);
+        },
+      ),
+      MenuItem.separator(),
+    ],
+  );
 }
