9e9b0d2eff76343a07409786d4502a068ffb7e8c
TDuffinNTU <thomaslukeduffin@hotmail.co.uk>
2022-06-21 09:18:41 +0100

    renamed file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_app/screens/home_Screen.dart';
+import 'package:my_app/screens/home_screen.dart';
 
 void main() {
   runApp(
